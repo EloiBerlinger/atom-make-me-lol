5df7bfc88129188df668f9d817c2337a620b652e
josex2r <codex2r@gmail.com>
2016-06-08 19:50:04 +0200

    First commit

---
+++ spec/atom-make-me-lol-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomMakeMeLolView from '../lib/atom-make-me-lol-view';
+
+describe('AtomMakeMeLolView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
